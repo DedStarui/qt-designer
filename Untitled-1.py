6319c7ba83fe37277536fd5b4f17d7e5250d4a25
DedStarui <136914518+DedStarui@users.noreply.github.com>
2024-12-28 16:51:58 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -9,13 +9,15 @@ class Widget(QMainWindow):
         self.ui.setupUi(self)
         self.load_data()
 
-
+        #Не правильно підключив функції до кнопок
+        # можна було відкрити файл ui.py і подивитися
+        # яка кнопка за що відповідає
         self.ui.pushButton.clicked.connect(self.add_note)
-        self.ui.pushButton_2.clicked.connect(self.del_note)
-        self.ui.pushButton_3.clicked.connect(self.save_note)
-        self.ui.pushButton_7.clicked.connect(self.add_tag)
-        self.ui.pushButton.clicked.connect(self.delete_tag)
-        self.ui.pushButton.clicked.connect(self.search_by_tag)
+        self.ui.pushButton_3.clicked.connect(self.del_note)
+        self.ui.pushButton_2.clicked.connect(self.save_note)
+        self.ui.pushButton_4.clicked.connect(self.add_tag)
+        self.ui.pushButton_5.clicked.connect(self.delete_tag)
+        self.ui.pushButton_6.clicked.connect(self.search_by_tag)
 
         self.ui.listWidget.itemClicked.connect(self.show_note)
     
@@ -35,7 +37,7 @@ class Widget(QMainWindow):
         if ok and note_name != "":
             if note_name not in self.notes:
                 self.notes[note_name] = {"текст": "", "теги": []}
-                self.ui.listWidget.addItems(note_name)
+                self.ui.listWidget.addItem(note_name) #Ми додаемо 1 елемент у тебе було Items
                 self.ui.textEdit.clear()
             else:
                 self.ui.textEdit.setPlainText(self.notes[note_name]["текст"])
@@ -51,15 +53,15 @@ class Widget(QMainWindow):
             else:
                 self.ui.textEdit.clear()
             self.ui.listWidget_2.clear()
-            self.ui.listWidget_2.additems(self.notes[key]["теги"])
+            self.ui.listWidget_2.addItems(self.notes[key]["теги"]) # addItems було з маленької букви
     def save_note(self):
         if self.ui.listWidget.selectedItems():
             key = self.ui.listWidget.selectedItems()[0].text()
             self.notes[key]["текст"] = self.ui.textEdit.toPlainText()
             self.ui.listWidget_2.clear()
             self.ui.listWidget_2.addItems(self.notes[key]["теги"])
-            self.ui.listWidget_2.clear()
-            self.ui.listWidget_2.addItems(self.notes.keys())
+            self.ui.listWidget.clear() # нам треба оновити список заміток, а в тебе було оновлення тегів
+            self.ui.listWidget.addItems(self.notes.keys()) # тобто ідентично 61 та 62 рядкам
             with open("notes_data.json", "w", encoding = "utf-8")as file:
                 json.dump(self.notes, file, sort_keys=True, ensure_ascii=False)
         else:
@@ -97,7 +99,7 @@ class Widget(QMainWindow):
             tag = self.ui.listWidget_2.selectedItems()[0].text()
             self.notes[key]["теги"].remove(tag)
             self.ui.listWidget_2.clear()
-            self.ui.lineWidget_2.addItems(self.notes[key]["теги"])
+            self.ui.listWidget_2.addItems(self.notes[key]["теги"]) # випадково написа line замість list
             with open("notes_data.json", "w", encoding = "utf-8") as file:
                 json.dump(self.notes,file,sort_keys=True)
         else:
@@ -108,7 +110,7 @@ class Widget(QMainWindow):
         if ok and tag!="":
             self.ui.listWidget.clear()
             self.ui.textEdit.clear()
-            matching_notes = [note for note, data in self.note.items() if tag in data["теги"]]
+            matching_notes = [note for note, data in self.notes.items() if tag in data["теги"]] # загубив s у self.notes
             self.ui.listWidget.addItems(matching_notes)
         else:
             print("Помилка під час пошуку")
@@ -120,6 +122,8 @@ app = QApplication([])
 ex = Widget()
 ex.show()
 app.exec_()
+
+
 
 
 
